e7f577a6175909ba300aa34388dba01c6b73ed9f
yaelenstack <danyael.delacruz@enstack.com>
2024-08-16 22:35:56 +0800

    Initial Commit

---
+++ money-monkey/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MoneyMonkey',
+  webDir: 'dist'
+};
+
+export default config;
